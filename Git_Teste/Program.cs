3c3b4e99697b52fd07b85668cc9d160f74d573c0
Augusto Santos <augustosantoscordeiro@gmail.com>
2020-06-16 11:28:56 -0300

    Alteracao da mensagem de bom dia para tenha um bom dia

--- Git_Teste/Program.cs
+++ Git_Teste/Program.cs
@@ -7,7 +7,7 @@ namespace Git_Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Tenha um bom dia");
             Console.WriteLine("Como você está?");
         }
     }
